fc86a84f2650c3a72d472e9089557f366e81c962
KIRUBAKARAN K <kirubakaran02002@gmail.com>
2025-09-27 07:19:38 +0530

    Create: Home page navbar created

---
+++ src/Components/Navbar.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import '../Styles/Navbar.css';
+import Logo from '../assets/Logo.jpg';
+const Navbar = () => {
+  const images = [
+    "../assets/Image1.jpeg",
+    "../assets/Image2.jpeg",
+    "../assets/Image3.jpeg",
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [menuOpen, setMenuOpen] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % images.length);
+    }, 5000); // 5000ms = 5 seconds
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+   <nav className="navbar text-white p-4 flex justify-between">
+    <div className="Logo">
+    <img src={Logo} alt="Logo" className="logo"/>
+      <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        &#9776;
+      </div>
+    <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/portfolio">Portfolio</Link>
+      <Link to="/videos">Videos</Link>
+      <Link to="/contact">Contact</Link> 
+    </div>
+    
+    </div>
+    <div className="navbar-center">
+      <h1>VELNAMBI VISUALS</h1>
+      <p> About Carnivals</p>
+      <div className="navbar-center-social-icons">
+      <a href="https://www.instagram.com/velnambi.visuals/" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-instagram"></i>
+      </a>
+      <a href="https://www.facebook.com/velnambi.visuals" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-facebook"></i>
+      </a>
+      <a href="https://www.youtube.com/velnambi.visuals" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-youtube"></i>
+      </a>
+      </div>
+    </div>
+</nav>
+
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="text-center">
+        <h2 className="text-4xl font-bold mb-4">Welcome to My Photography</h2>
+        <p className="text-lg text-white-600">Capturing moments that last forever</p>
+      </div>
+    );
+  }
+  
